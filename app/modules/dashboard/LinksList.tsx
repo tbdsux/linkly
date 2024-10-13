5a393f655932abb8a2c9ddf83a0b9c85b94cfa7e
Joshue Abance <iamcoderx@gmail.com>
2024-10-13 23:18:03 +0800

    feat: implement deletion of links

--- app/modules/dashboard/LinksList.tsx
+++ app/modules/dashboard/LinksList.tsx
@@ -1,4 +1,6 @@
+import { EditIcon } from "lucide-react";
 import { Badge } from "~/components/ui/badge";
+import { Button } from "~/components/ui/button";
 import {
   Card,
   CardContent,
@@ -7,6 +9,7 @@ import {
   CardTitle,
 } from "~/components/ui/card";
 import { useDashboard } from "~/providers/DashboardProvider";
+import DeleteLink from "./DeleteLink";
 
 export default function DashboardLinksList() {
   const { linksQuery } = useDashboard();
@@ -18,7 +21,18 @@ export default function DashboardLinksList() {
       {linksQuery.isSuccess && (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           {linksQuery.data?.documents.map((link) => (
-            <Card key={link.$id} className="">
+            <Card key={link.$id} className="group relative">
+              <div className="absolute top-2 right-2 hidden group-hover:block group-focus:block">
+                <div className="space-x-2">
+                  <Button className="text-sm h-auto py-1 space-x-1 px-2">
+                    <EditIcon className="h-4 w-4" />
+                    <span>Edit</span>
+                  </Button>
+
+                  <DeleteLink linkTitle={link.urlTitle} id={link.$id} />
+                </div>
+              </div>
+
               <CardHeader>
                 <CardTitle className="text-lg inline-flex space-x-2">
                   {link.urlFavicon ? (
