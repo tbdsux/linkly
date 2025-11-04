83eb7aedd9a9b6ca0ef276181d6af524c87220ab
Joshue Abance <iamcoderx@gmail.com>
2025-11-04 12:07:59 +0800

    use default build setup for appwrite deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
-import { nitro } from 'nitro/vite'
 import { defineConfig } from 'vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 
@@ -13,7 +12,6 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart(),
-    nitro(),
     viteReact(),
   ],
 })
